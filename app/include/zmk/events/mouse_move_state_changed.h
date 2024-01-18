bryanforbes/zmk

feat(mouse): Add mouse move and scroll support [WIP]
Co-authored-by: Alexander Krikun <krikun98@gmail.com>
Co-authored-by: Robert U <urob@users.noreply.github.com>
Co-authored-by: Shawn Meier <ftc@users.noreply.github.com>

app/include/zmk/events/mouse_move_state_changed.h
@@ -1,0 +1,33 @@
+
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
+#include <zmk/mouse.h>
+
+struct zmk_mouse_move_state_changed {
+    struct vector2d max_speed;
+    struct mouse_config config;
+    bool state;
+    int64_t timestamp;
+};
+
+ZMK_EVENT_DECLARE(zmk_mouse_move_state_changed);
+
+static inline struct zmk_mouse_move_state_changed_event *
+zmk_mouse_move_state_changed_from_encoded(uint32_t encoded, struct mouse_config config,
+                                          bool pressed, int64_t timestamp) {
+    struct vector2d max_speed = (struct vector2d){
+        .x = MOVE_HOR_DECODE(encoded),
+        .y = MOVE_VERT_DECODE(encoded),
+    };
+
+    return new_zmk_mouse_move_state_changed((struct zmk_mouse_move_state_changed){
+        .max_speed = max_speed, .config = config, .state = pressed, .timestamp = timestamp});
+}

app/include/zmk/events/mouse_scroll_state_changed.h
@@ -1,0 +1,34 @@
+
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
+#include <zmk/mouse.h>
+#include <dt-bindings/zmk/mouse.h>
+
+struct zmk_mouse_scroll_state_changed {
+    struct vector2d max_speed;
+    struct mouse_config config;
+    bool state;
+    int64_t timestamp;
+};
+
+ZMK_EVENT_DECLARE(zmk_mouse_scroll_state_changed);
+
+static inline struct zmk_mouse_scroll_state_changed_event *
+zmk_mouse_scroll_state_changed_from_encoded(uint32_t encoded, struct mouse_config config,
+                                            bool pressed, int64_t timestamp) {
+    struct vector2d max_speed = (struct vector2d){
+        .x = SCROLL_HOR_DECODE(encoded),
+        .y = SCROLL_VERT_DECODE(encoded),
+    };
+
+    return new_zmk_mouse_scroll_state_changed((struct zmk_mouse_scroll_state_changed){
+        .max_speed = max_speed, .config = config, .state = pressed, .timestamp = timestamp});
+}

app/src/behaviors/behavior_mouse_move.c
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_mouse_move
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/behavior.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/mouse_move_state_changed.h>
+#include <zmk/mouse.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_mouse_move_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = device_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(
+        zmk_mouse_move_state_changed_from_encoded(binding->param1, *config, true, event.timestamp));
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = device_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_move_state_changed_from_encoded(binding->param1, *config,
+                                                                     false, event.timestamp));
+}
+
+static const struct behavior_driver_api behavior_mouse_move_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+
+#define KP_INST(n)                                                                                 \
+    static struct mouse_config behavior_mouse_move_config_##n = {                                  \
+        .delay_ms = DT_INST_PROP(n, delay_ms),                                                     \
+        .time_to_max_speed_ms = DT_INST_PROP(n, time_to_max_speed_ms),                             \
+        .acceleration_exponent = DT_INST_PROP(n, acceleration_exponent),                           \
+    };                                                                                             \
+    DEVICE_DT_INST_DEFINE(n, behavior_mouse_move_init, NULL, NULL,                                 \
+                          &behavior_mouse_move_config_##n, APPLICATION,                            \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_mouse_move_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */

app/src/behaviors/behavior_mouse_scroll.c
@@ -1,0 +1,58 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_mouse_scroll
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/events/mouse_scroll_state_changed.h>
+#include <zmk/behavior.h>
+#include <zmk/hid.h>
+#include <zmk/endpoints.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_mouse_scroll_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = device_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_scroll_state_changed_from_encoded(binding->param1, *config,
+                                                                       true, event.timestamp));
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = device_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_scroll_state_changed_from_encoded(binding->param1, *config,
+                                                                       false, event.timestamp));
+}
+
+static const struct behavior_driver_api behavior_mouse_scroll_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+
+#define KP_INST(n)                                                                                 \
+    static struct mouse_config behavior_mouse_scroll_config_##n = {                                \
+        .delay_ms = DT_INST_PROP(n, delay_ms),                                                     \
+        .time_to_max_speed_ms = DT_INST_PROP(n, time_to_max_speed_ms),                             \
+        .acceleration_exponent = DT_INST_PROP(n, acceleration_exponent),                           \
+    };                                                                                             \
+    DEVICE_DT_INST_DEFINE(n, behavior_mouse_scroll_init, NULL, NULL,                               \
+                          &behavior_mouse_scroll_config_##n, APPLICATION,                          \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_mouse_scroll_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */

zemackdaddy/zmk-moergo

Co-authored-by: Seth Milliken <seth@araxia.net>

app/include/zmk/events/mouse_move_state_changed.h
@@ -1,0 +1,33 @@
+
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
+#include <zmk/mouse.h>
+
+struct zmk_mouse_move_state_changed {
+    struct vector2d max_speed;
+    struct mouse_config config;
+    bool state;
+    int64_t timestamp;
+};
+
+ZMK_EVENT_DECLARE(zmk_mouse_move_state_changed);
+
+static inline struct zmk_mouse_move_state_changed_event *
+zmk_mouse_move_state_changed_from_encoded(uint32_t encoded, struct mouse_config config,
+                                          bool pressed, int64_t timestamp) {
+    struct vector2d max_speed = (struct vector2d){
+        .x = MOVE_X_DECODE(encoded),
+        .y = MOVE_Y_DECODE(encoded),
+    };
+
+    return new_zmk_mouse_move_state_changed((struct zmk_mouse_move_state_changed){
+        .max_speed = max_speed, .config = config, .state = pressed, .timestamp = timestamp});
+}

app/include/zmk/events/mouse_scroll_state_changed.h
@@ -1,0 +1,34 @@
+
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
+#include <zmk/mouse.h>
+#include <dt-bindings/zmk/mouse.h>
+
+struct zmk_mouse_scroll_state_changed {
+    struct vector2d max_speed;
+    struct mouse_config config;
+    bool state;
+    int64_t timestamp;
+};
+
+ZMK_EVENT_DECLARE(zmk_mouse_scroll_state_changed);
+
+static inline struct zmk_mouse_scroll_state_changed_event *
+zmk_mouse_scroll_state_changed_from_encoded(uint32_t encoded, struct mouse_config config,
+                                            bool pressed, int64_t timestamp) {
+    struct vector2d max_speed = (struct vector2d){
+        .x = SCRL_X_DECODE(encoded),
+        .y = SCRL_Y_DECODE(encoded),
+    };
+
+    return new_zmk_mouse_scroll_state_changed((struct zmk_mouse_scroll_state_changed){
+        .max_speed = max_speed, .config = config, .state = pressed, .timestamp = timestamp});
+}

app/src/behaviors/behavior_mouse_move.c
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_mouse_move
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/behavior.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/mouse_move_state_changed.h>
+#include <zmk/mouse.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_mouse_move_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(
+        zmk_mouse_move_state_changed_from_encoded(binding->param1, *config, true, event.timestamp));
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_move_state_changed_from_encoded(binding->param1, *config,
+                                                                     false, event.timestamp));
+}
+
+static const struct behavior_driver_api behavior_mouse_move_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+
+#define KP_INST(n)                                                                                 \
+    static struct mouse_config behavior_mouse_move_config_##n = {                                  \
+        .delay_ms = DT_INST_PROP(n, delay_ms),                                                     \
+        .time_to_max_speed_ms = DT_INST_PROP(n, time_to_max_speed_ms),                             \
+        .acceleration_exponent = DT_INST_PROP(n, acceleration_exponent),                           \
+    };                                                                                             \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_mouse_move_init, NULL, NULL,                               \
+                          &behavior_mouse_move_config_##n, APPLICATION,                            \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_mouse_move_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */

app/src/behaviors/behavior_mouse_scroll.c
@@ -1,0 +1,58 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_mouse_scroll
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/events/mouse_scroll_state_changed.h>
+#include <zmk/behavior.h>
+#include <zmk/hid.h>
+#include <zmk/endpoints.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_mouse_scroll_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_scroll_state_changed_from_encoded(binding->param1, *config,
+                                                                       true, event.timestamp));
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
+    const struct mouse_config *config = dev->config;
+    return ZMK_EVENT_RAISE(zmk_mouse_scroll_state_changed_from_encoded(binding->param1, *config,
+                                                                       false, event.timestamp));
+}
+
+static const struct behavior_driver_api behavior_mouse_scroll_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+
+#define KP_INST(n)                                                                                 \
+    static struct mouse_config behavior_mouse_scroll_config_##n = {                                \
+        .delay_ms = DT_INST_PROP(n, delay_ms),                                                     \
+        .time_to_max_speed_ms = DT_INST_PROP(n, time_to_max_speed_ms),                             \
+        .acceleration_exponent = DT_INST_PROP(n, acceleration_exponent),                           \
+    };                                                                                             \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_mouse_scroll_init, NULL, NULL,                             \
+                          &behavior_mouse_scroll_config_##n, APPLICATION,                          \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_mouse_scroll_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */